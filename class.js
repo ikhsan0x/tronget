41130173bcade2faabe76d33fb77ae2249b542eb
ikhsan0x <89199835+ikhsan0x@users.noreply.github.com>
2023-08-18 21:02:00 +0700

    Update class.js

--- class.js
+++ class.js
@@ -4,45 +4,47 @@ class Tron {
 
     constructor(node, apikey) {
         this.mainTronWeb = new TronWeb(node, node, node);
-	this.mainTronWeb.setHeader({ "TRON-PRO-API-KEY": apikey });
+        this.mainTronWeb.setHeader({
+            "TRON-PRO-API-KEY": apikey
+        });
     }
 
     async coinBalance(address) {
-        try{
+        try {
             const balanceInSun = await this.mainTronWeb.trx.getBalance(address);
             const balanceInTRX = this.mainTronWeb.fromSun(balanceInSun);
             return nResult(balanceInTRX);
         }
-        catch(e){
-			return nError(e);
+        catch (e) {
+            return nError(e);
         }
     }
 
     async tokenBalance(token, address) {
-        try{
+        try {
             const tokenContract = await this.mainTronWeb.contract().at(token);
             const balanceWei = await tokenContract.balanceOf(address).call();
             const decimal = await tokenContract.decimals().call();
-            const balanceInToken = this.mainTronWeb.fromSun( balanceWei.div( Math.pow( 10, decimal ) ) );
+            const balanceInToken = this.mainTronWeb.fromSun(balanceWei.div(Math.pow(10, decimal)));
             return nResult(balanceInToken);
         }
-        catch(e){
-			return nError(e);
+        catch (e) {
+            return nError(e);
         }
-    } 
+    }
 
-	async totalSupply(token) {
-		try{
-			const tokenContract = await this.mainTronWeb.contract().at(token);
-			const totalSupplyInWei = await tokenContract.totalSupply().call();
-			let totalSupply =  this.mainTronWeb.fromSun(totalSupplyInWei);
-			return nResult(totalSupply);
-		}
-		catch(e){
-			return nError(e);
-		}
-	}   
-     
+    async totalSupply(token) {
+        try {
+            const tokenContract = await this.mainTronWeb.contract().at(token);
+            const totalSupplyInWei = await tokenContract.totalSupply().call();
+            let totalSupply = this.mainTronWeb.fromSun(totalSupplyInWei);
+            return nResult(totalSupply);
+        }
+        catch (e) {
+            return nError(e);
+        }
+    }
+
 }
 module.exports = Tron;
 
@@ -50,14 +52,14 @@ function nError(e) {
     const response = {
         result: false,
         message: e
-    };	
-    return response;    
+    };
+    return response;
 }
 
 function nResult(r) {
     const response = {
         result: true,
         message: r
-    };	
-    return response;    
+    };
+    return response;
 }
