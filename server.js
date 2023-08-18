13035f4d9a15382c805e4606daf39e911ff688ce
ikhsan0x <89199835+ikhsan0x@users.noreply.github.com>
2023-08-18 21:03:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,111 +5,119 @@ const Tron = require('./class.js');
 const app = express();
 const HTTP_PORT = 8008;
 app.disable('x-powered-by');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'application/json');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    next();
 });
 
-const fullNode = 'https://api.trongrid.io'; //mainnet
-
 app.get("/getTrxBalance", async function(req, res, next) {
-	try {	
-		let errors=[];
-		if (!req.query.apikey){
-			errors.push("No apikey specified");
-		}  		
-		if (!req.query.address){
-			errors.push("No address specified");
-		}		
-		if (errors.length){
-			res.status(400).json({"error":errors.join(",")});
-			return;
-		}		
+    try {
+        let errors = [];
+        if (!req.query.apikey) {
+            errors.push("No apikey specified");
+        }
+        if (!req.query.address) {
+            errors.push("No address specified");
+        }
+        if (errors.length) {
+            res.status(400).json({
+                "error": errors.join(",")
+            });
+            return;
+        }
 
-        	let apikey = req.query.apikey;
-		let address = req.query.address;
+        let apikey = req.query.apikey;
+        let address = req.query.address;
 
-		const trons = new Tron(fullNode, apikey);
-		let result = await trons.coinBalance(address);
+        const trons = new Tron(apikey);
+        let result = await trons.coinBalance(address);
 
-		res.json(result);
-	}
-	catch(e){
-		return res.status(401).send({
-			message : e.message
-		});
-	}	
+        res.json(result);
+    }
+    catch (e) {
+        return res.status(401).send({
+            message: e.message
+        });
+    }
 });
 
 app.get("/getTrc20Balance", async function(req, res, next) {
-	try {	
-		let errors=[];
-		if (!req.query.apikey){
-			errors.push("No apikey specified");
-		} 		
-		if (!req.query.token){
-			errors.push("No token specified");
-		}                
-		if (!req.query.address){
-			errors.push("No address specified");
-		}
-		if (errors.length){
-			res.status(400).json({"error":errors.join(",")});
-			return;
-		}		
+    try {
+        let errors = [];
+        if (!req.query.apikey) {
+            errors.push("No apikey specified");
+        }
+        if (!req.query.token) {
+            errors.push("No token specified");
+        }
+        if (!req.query.address) {
+            errors.push("No address specified");
+        }
+        if (errors.length) {
+            res.status(400).json({
+                "error": errors.join(",")
+            });
+            return;
+        }
 
-        	let apikey = req.query.apikey;
-		let token = req.query.token;
-		let address = req.query.address;
+        let apikey = req.query.apikey;
+        let token = req.query.token;
+        let address = req.query.address;
 
-		const trons = new Tron(fullNode, apikey);
-		let result = await trons.tokenBalance(token, address);
+        const trons = new Tron(apikey);
+        let result = await trons.tokenBalance(token, address);
 
-		res.json(result);
-	}
-	catch(e){
-		return res.status(401).send({
-			message : e.message
-		});
-	}	
+        res.json(result);
+    }
+    catch (e) {
+        return res.status(401).send({
+            message: e.message
+        });
+    }
 });
 
 app.get("/getTotalSupply", async function(req, res, next) {
-	try {	
-		let errors=[];
-		if (!req.query.apikey){
-			errors.push("No apikey specified");
-		} 		
-		if (!req.query.token){
-			errors.push("No token specified");
-		}                
-		if (errors.length){
-			res.status(400).json({"error":errors.join(",")});
-			return;
-		}		
+    try {
+        let errors = [];
+        if (!req.query.apikey) {
+            errors.push("No apikey specified");
+        }
+        if (!req.query.token) {
+            errors.push("No token specified");
+        }
+        if (errors.length) {
+            res.status(400).json({
+                "error": errors.join(",")
+            });
+            return;
+        }
 
-        	let apikey = req.query.apikey;
-		let token = req.query.token;
+        let apikey = req.query.apikey;
+        let token = req.query.token;
 
-		const trons = new Tron(fullNode, apikey);
-		let result = await trons.totalSupply(token);
+        const trons = new Tron(apikey);
+        let result = await trons.totalSupply(token);
 
-		res.json(result);
-	}
-	catch(e){
-		return res.status(401).send({
-			message : e.message
-		});
-	}	
+        res.json(result);
+    }
+    catch (e) {
+        return res.status(401).send({
+            message: e.message
+        });
+    }
 });
 
 app.get("/", (req, res, next) => {
-    res.json({"status":"running"})
+    res.json({
+        "status": "running"
+    })
 });
 
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT));
+    console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT));
 });
